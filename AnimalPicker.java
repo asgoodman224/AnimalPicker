08369f37e6b1e24401e4ed20d41fc8439fcc3adf
asgoodman224 <157658323+asgoodman224@users.noreply.github.com>
2024-02-28 12:29:26 -0500

    step 4 with the dog and cat art

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -8,15 +8,21 @@ public class AnimalPicker {
         boolean isValidInput = false;
 
         while (!isValidInput) {
-            System.out.println("Which animal would you like to see: a dog, a cat, or a fish?");
+            System.out.println("Which animal would you like to see: a dog or a cat?");
             System.out.print("Enter your choice: ");
             userInput = scanner.nextLine().toLowerCase();
             
             if (userInput.equals("dog")) {
-                System.out.println("DOG ASCII ART HERE");
+                System.out.println("^..^      /\r\n" + //
+                                        "/_/\\_____/\r\n" + //
+                                        "   /\\   /\\\r\n" + //
+                                        "  /  \\ /  \\");
                 isValidInput = true;
             } else if (userInput.equals("cat")) {
-                System.out.println("CAT ASCII ART HERE");
+                System.out.println("|\\---/|\r\n" + //
+                                        "| o_o |\r\n" + //
+                                        " \\_^_/\r\n" + //
+                                        "");
                 isValidInput = true;
             } else {
                 System.out.println("Invalid option. Please choose between dog or cat");
