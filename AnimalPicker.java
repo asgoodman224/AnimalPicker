c864120e66eb9beabeb6878037efe9dad807a09d
asgoodman224 <157658323+asgoodman224@users.noreply.github.com>
2024-02-28 12:25:01 -0500

    steps 2 and 3

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -2,15 +2,29 @@ import java.util.Scanner;
 
 public class AnimalPicker {
     public static void main(String[] args) {
-        @SuppressWarnings("resource")
         Scanner scanner = new Scanner(System.in);
         
+        String userInput;
         boolean isValidInput = false;
 
         while (!isValidInput) {
             System.out.println("Which animal would you like to see: a dog, a cat, or a fish?");
             System.out.print("Enter your choice: ");
-            scanner.nextLine().toLowerCase();
+            userInput = scanner.nextLine().toLowerCase();
+            
+            if (userInput.equals("dog")) {
+                System.out.println("DOG ASCII ART HERE");
+                isValidInput = true;
+            } else if (userInput.equals("cat")) {
+                System.out.println("CAT ASCII ART HERE");
+                isValidInput = true;
+            } else {
+                System.out.println("Invalid option. Please choose between dog or cat");
+            }
         }
+        
+        scanner.close();
     }
 }
+
+
